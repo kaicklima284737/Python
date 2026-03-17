4d819c2da6b6e3bee303b0d4c21f17954e9a3e88
Laboratório FIAP <labsfiap@fiap.com.br>
2026-03-16 22:42:47 -0300

    first commit

---
+++ Aula 03.py
@@ -0,0 +1,58 @@
+#Exercício 01
+
+
+"""
+CP1 = float(input("Informe a nota do cp1: "))
+CP2 = float(input("Informe a nota do cp2: "))
+Sprint1 = float(input("informe a nota do Sprint1: "))
+Sprint2 = float(input("informe a nota do Sprint2: "))
+GS1 =  float(input("informe a nota do GS1: "))
+
+M1 = ((CP1+CP2)/2)*0.2 + ((Sprint1+Sprint2)/2)*0.2 + GS1*0.6
+
+print ("Sua média é:", M1)
+
+
+#Exercicío 02
+
+
+Valor = float(input("Infore o valor do Produto:R$ "))
+
+Valor_Desconto = Valor - Valor*0.12
+
+#print("O preço do produto com desconto é R$ ",Valor_Desconto)
+print(f"O preço do produto com desconto é R$ {Valor_Desconto:.2f}")
+
+
+#Exercicío 03
+
+
+Int_Segundos = float(input("Insira um valor inteiro em segundos: "))
+
+Horas = Int_Segundos // 3600
+
+Minutos = (Int_Segundos // 60) % 60
+
+segundos = Int_Segundos % 60
+
+print(f"O valor de {Int_Segundos} é igual a {Horas} Horas,{Minutos} Minutos e {segundos} Segundos f")
+
+
+
+
+#Exercício 4
+
+
+# Chip pé direito Identificação, Identificação R$0.40
+# 2 Chips pé esquerdo Alimento R$0.35
+
+Franja = float(input("Quantos Frangos Foram Marcados: "))
+
+PE = (Franja * 2) * 0.35
+PD = Franja * 0.40
+
+Gasto = PE + PD
+
+print(f"O valor gasto total na Granja foi de R${Gasto:.2f}")
+"""
+
